dae76e161c2fe6f017773456f4c697a1ab4c33d8
Pravin Patil <pravinsaindane2005@gmail.com>
2025-06-25 11:51:45 +0530

    Add Settings

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -173,7 +173,7 @@ EMAIL_HOST_PASSWORD = 'weflnvhnjinjnjnd'
 # hosing releted
 
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Whitenoise for static files
