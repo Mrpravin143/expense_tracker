e28b5d4b11010927988b8b45cfdc0e6a923cc716
Pravin Patil <pravinsaindane2005@gmail.com>
2025-07-21 15:43:05 +0530

    Configure database settings

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -68,8 +68,11 @@ ASGI_APPLICATION = 'firstproject.asgi.application'
 #     }
 # }
 
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://testdatabase_vvdx_user:y2eyRCEgsxWwDyoXkhwLDoKEbjvWtU6X@dpg-d1v0ts95pdvs73cpm4l0-a/testdatabase_vvdx")
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 }
 
 
